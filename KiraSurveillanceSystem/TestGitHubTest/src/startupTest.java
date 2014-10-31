e05e45bc15c6093eb1e44870b0809c804b7cdf75
markhinnen <mark@hinnen.info>
2014-10-31 22:59:03 +0100

    First Junit Test Package

---
+++ KiraSurveillanceSystem/TestGitHubTest/src/startupTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class startupTest {
+	 @Test
+	 public void startupDummyTest() {
+		 assertEquals(1,1,0);
+		 
+		 startup.main(null);
+	 }
+}
