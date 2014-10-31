1089c15a023936d286d4f71ffe63b6f3698ff855
markhinnen <mark@hinnen.info>
2014-10-31 22:42:24 +0100

    GitTest 2

--- KiraSurveillanceSystem/TestGitHub/src/startup.java
+++ KiraSurveillanceSystem/TestGitHub/src/startup.java
@@ -3,7 +3,7 @@ public class startup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first output v2");
+		System.out.println("first output");
 	}
 
 }
